3700ae8353d5ac911e7786de9e409ff31ad0da07
Arthur Teles <arthurnoyesit@gmail.com>
2024-12-20 10:52:16 -0300

    mobile 4

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   };
 
   return (
-    <div className={`flex flex-col h-[100dvh] ${isFullScreen ? 'overflow-hidden' : ''}`}>
+    <div className={`flex flex-col h-[100dvh]`}>
       {/* O Header só será exibido se não estiver em tela cheia */}
       {!isFullScreen && <Header />}
       
